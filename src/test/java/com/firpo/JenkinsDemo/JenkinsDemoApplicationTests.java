d0216095c0bd8018d2e2b0b83c7cc3a0e0efda96
PabluxFirpux <pfirpomolina@gmail.com>
2024-11-25 15:18:31 +0100

    Demo

--- src/test/java/com/firpo/JenkinsDemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/firpo/JenkinsDemo/JenkinsDemoApplicationTests.java
@@ -3,11 +3,29 @@ package com.firpo.JenkinsDemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class JenkinsDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void checkMathsAreStillReal() {
+		int x = 5+5;
+		int y = 6+1;
+		assertEquals(x,10);
+		assertEquals(y,7);
+	}
+
+	@Test
+	void checkMathsAreStillRealTheSequel() {
+		int x = 5+5;
+		int y = 6+1;
+		assertEquals(x,11);
+		assertEquals(y,7);
 	}
 
 }
